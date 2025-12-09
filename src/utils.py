3544c00c43c7dfa3f056c4cbce7cbff09dbfec9f
Vaishnavi Patil <116648404+vaishnavibpatil@users.noreply.github.com>
2025-12-09 14:04:38 +0530

    Add comments to utils.py for clarity
    Added comments to clarify the purpose of imports and code functionality.

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,12 @@
 # src/utils.py
-import requests
-import json
+
+import requests                 # For making HTTP request to API
+import json                     # For converting response to JSON format
 
 def get_location():
-    response = requests.get('http://ip-api.com/json')
-    data = json.loads(response.text)
-    return data['lat'], data['lon']
+
+    response = requests.get('http://ip-api.com/json')   # Call IP-based location API
+
+    data = json.loads(response.text)                    # Convert API response to JSON
+
+    return data['lat'], data['lon']                     # Return latitude and longitude
